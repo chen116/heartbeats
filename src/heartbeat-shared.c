584b80bbd7a3c85a88f04824f53cfec0b3ec12e7
Victor Chen <yvictorck@gmail.com>
2018-07-17 19:03:33 -0400

    a

--- src/heartbeat-shared.c
+++ src/heartbeat-shared.c
@@ -35,7 +35,7 @@ heartbeat_t* heartbeat_init(int64_t window_size,
   // int pid = getpid();
   int pid;
   char loglog[8];
-  sscanf( log_name, "%s%d", loglog, &pid );
+  sscanf( log_name, "%[^_]%s_%[^_]%d", loglog, &pid );
   printf("heartbeat init with %s with pid %d\n",pid );
 
 
